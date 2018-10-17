95b58c64a464f0fd5ce32c4f0d1fe064a7be603d
rmorenot <rmorenot@cisco.com>
2018-10-17 17:25:06 -0500

    AboutPrimitives fixing line 186 specifying f type

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -183,8 +183,8 @@ public class AboutPrimitives {
 
     @Koan
     public void floatsHaveASmallerRangeThanDoubles() {
-        assertEquals(Float.MIN_VALUE, 1.4E-45);
-        assertEquals(Float.MAX_VALUE, 3.4028235E38);
+        assertEquals(Float.MIN_VALUE, 1.4E-45f);
+        assertEquals(Float.MAX_VALUE, 3.4028235E38f);
     }
 
     @Koan
@@ -197,7 +197,7 @@ public class AboutPrimitives {
     }
 
     private Class<?> getType(long value) {
-        return long.class;
+        return long.class;  
     }
 
     private Class<?> getType(float value) {
