4a7ac610b204ca284a1928ae841b4d78ade45114
rmorenot <rmorenot@cisco.com>
2018-10-18 15:10:14 -0500

    AboutObjects line 25 .class

--- koans/src/beginner/AboutObjects.java
+++ koans/src/beginner/AboutObjects.java
@@ -22,8 +22,8 @@ public class AboutObjects {
         }
 
         Class<?>[] ancestors = getAncestors(new Foo());
-        assertEquals(ancestors[0], true);
-        assertEquals(ancestors[1], true);
+        assertEquals(ancestors[0], Foo.class);
+        assertEquals(ancestors[1], Foo.class);
     }
 
     @Koan
