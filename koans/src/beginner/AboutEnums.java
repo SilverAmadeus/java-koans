b329a6ebbece6b900ec6c4ef525c7300ce5ed3a3
rmorenot <rmorenot@cisco.com>
2018-10-19 17:08:00 -0500

    AboutEnums line 47 again...

--- koans/src/beginner/AboutEnums.java
+++ koans/src/beginner/AboutEnums.java
@@ -42,7 +42,7 @@ public class AboutEnums {
         // value is private but we still can access it. Why?
         // Try moving the enum outside the AboutEnum class... What do you expect?
         // What happens?
-        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, 12);
+        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, true);
     }
 
     enum OpticalMedia {
