0c91dd5c468dd3656153d1fefda9fb868f58b1a9
rmorenot <rmorenot@cisco.com>
2018-10-29 12:35:39 -0600

    AboutTryResources

--- koans/src/java7/AboutTryWithResources.java
+++ koans/src/java7/AboutTryWithResources.java
@@ -47,7 +47,7 @@ public class AboutTryWithResources {
         } catch (IOException e) {
             line = "error";
         }
-        assertEquals(line, __);
+        assertEquals(line, "first line\nsecond line");
     }
 
     @Koan
@@ -60,7 +60,7 @@ public class AboutTryWithResources {
         } catch (FileNotFoundException e) {
             line = "no more leaking!";
         }
-        assertEquals(line, __);
+        assertEquals(line, "no more leaking");
     }
 
     @Koan
@@ -83,7 +83,7 @@ public class AboutTryWithResources {
         } catch (IOException e) {
             line = "error";
         }
-        assertEquals(line, __);
+        assertEquals(line, "first line\nsecond line");
     }
 
     @Koan
